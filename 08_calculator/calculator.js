e803bb60961ddc9df868db8ffe8a6e064e5f527f
MissCoffee27 <coffee4apps@gmail.com>
2024-08-30 23:30:52 -0400

    Edited

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,38 +1,37 @@
-const add = function(a, b) {
+const add = function (a, b) {
   return a + b;
-	
 };
 
-const subtract = function(a, b) {
+const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function(array) {
-  return array.reduce((total, current) ==> total + current, 0);	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function(array) {
-  return array.reduce((product, current) ==> product * current);
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function(a, b) {
-  return Math.pow(a, b);	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function(n) {
+const factorial = function (n) {
   if (n === 0) return 1;
   let product = 1;
-  for (let i = n; i > 0; I--) {
+  for (let i = n; i > 0; i--) {
     product *= i;
-  return product;	
+  }
+  return product;
 };
 
-// Do not edit below this line
 module.exports = {
   add,
   subtract,
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
